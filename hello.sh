c20821201937f1436953b0730ba37a937321912f
aharon <aharon.smbatyan@gmail.com>
2024-03-19 23:28:31 +0400

    Added functionality for a bear

--- hello.sh
+++ hello.sh
@@ -8,6 +8,10 @@ dog() {
   echo "I am a dog"
 }
 
+bear() {
+  echo "I am a bear"
+}
+
 hello() {
   echo "hello man"
 }
@@ -16,6 +20,8 @@ if [ "$1" == "dog" ]; then
   dog
 elif [ "$1" == "cat" ]; then
   cat
+elif [ "$1" == "bear" ]; then
+  bear
 else
   hello $1
 fi
